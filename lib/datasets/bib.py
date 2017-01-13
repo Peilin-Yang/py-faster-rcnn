e4a42fe9da0d0e162cb53b2915c0767396fd6946
Peilin Yang <yangpeilyn@gmail.com>
2017-01-13 10:42:47 -0500

    add bib as another imdb

--- lib/datasets/bib.py
+++ lib/datasets/bib.py
@@ -29,6 +29,7 @@ class bib(imdb):
         self._classes = ('__background__', # always index 0
                          'bib')
         self._image_ext = '.jpg'
+        self._annotation_ext = '.xml'
         self._class_to_ind = dict(zip(self.classes, xrange(self.num_classes)))
         self._image_index = self._load_image_set_index()
         # Default to roidb handler
@@ -58,7 +59,7 @@ class bib(imdb):
         Construct an image path from the image's "index" identifier.
         """
         image_path = os.path.join(self._data_path, 'JPEGImages',
-                            self._image_set, index)
+                            self._image_set, index, self._image_ext)
         assert os.path.exists(image_path), \
                 'Path does not exist: {}'.format(image_path)
         return image_path
@@ -69,7 +70,7 @@ class bib(imdb):
         """
         # Example path to image set file:
         # self._bib_path + /500X500Gray/JPEGImages/trainging/
-        image_index = os.listdir(os.path.join(
+        image_index = [fn.split('.')[0] for fn in os.listdir(os.path.join(
                 self._data_path, 'JPEGImages', self._image_set))
         return image_index
 
@@ -92,7 +93,7 @@ class bib(imdb):
             print '{} gt roidb loaded from {}'.format(self.name, cache_file)
             return roidb
 
-        gt_roidb = [self._load_pascal_annotation(index)
+        gt_roidb = [self._load_annotation(index)
                     for index in self.image_index]
         with open(cache_file, 'wb') as fid:
             cPickle.dump(gt_roidb, fid, cPickle.HIGHEST_PROTOCOL)
@@ -166,12 +167,13 @@ class bib(imdb):
 
         return self.create_roidb_from_box_list(box_list, gt_roidb)
 
-    def _load_bib_annotation(self, index):
+    def _load_annotation(self, index):
         """
         Load image and bounding boxes info from XML file in the PASCAL VOC
         format.
         """
-        filename = os.path.join(self._data_path, 'Annotations', index + '.xml')
+        filename = os.path.join(self._data_path, 'Annotations', self._label, 
+                                    index + _annotation_ext)
         tree = ET.parse(filename)
         objs = tree.findall('object')
         if not self.config['use_diff']:
