a0516521d280623d0c3482e7eea9aeef5a043944
Peilin Yang <yangpeilyn@gmail.com>
2017-01-15 00:10:20 -0500

    bug fix

--- lib/fast_rcnn/config.py
+++ lib/fast_rcnn/config.py
@@ -215,7 +215,7 @@ def get_output_dir(imdb, net=None):
     A canonical path is built using the name from an imdb and a network
     (if not None).
     """
-    outdir = osp.abspath(osp.join(' /lustre/scratch/franklyn/photo_archive/Detection/output', __C.EXP_DIR, imdb.name))
+    outdir = osp.abspath(osp.join(__C.ROOT_DIR, 'output', __C.EXP_DIR, imdb.name))
     if net is not None:
         outdir = osp.join(outdir, net.name)
     if not os.path.exists(outdir):
