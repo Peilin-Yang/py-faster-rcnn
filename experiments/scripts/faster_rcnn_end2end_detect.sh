b7a29e10d6ec540450a0e772b3645ea67f871b30
Peilin Yang <yangpeilyn@gmail.com>
2017-01-21 13:03:03 -0500

    add script

--- experiments/scripts/faster_rcnn_end2end_detect.sh
+++ experiments/scripts/faster_rcnn_end2end_detect.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-
+#output/faster_rcnn_end2end/bib_500X500Gray_training/vgg16_faster_rcnn_iter_70000.caffemodel \
 time ./tools/detect_net.py --gpu 0 \
   --def models/bib/VGG16/faster_rcnn_end2end/test.prototxt \
-  --net $1 \ #output/faster_rcnn_end2end/bib_500X500Gray_training/vgg16_faster_rcnn_iter_70000.caffemodel \
+  --net $1 \
   --cfg experiments/cfgs/faster_rcnn_end2end.yml \
   --input $2 \
   --output $3 \
