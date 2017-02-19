43f3ee4315ca5298606796e4574cdd4a12dea730
Peilin Yang <yangpeilyn@gmail.com>
2017-02-18 22:54:03 -0500

    add alt_opt detect

--- experiments/scripts/faster_rcnn_alt_opt_detect.sh
+++ experiments/scripts/faster_rcnn_alt_opt_detect.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #output/faster_rcnn_end2end/bib_500X500Gray_training/vgg16_faster_rcnn_iter_70000.caffemodel \
 time ./tools/detect_net.py --gpu 0 \
-  --def models/bib/VGG16/faster_rcnn_alt_opt/test.prototxt \
+  --def models/bib/VGG16/faster_rcnn_alt_opt/faster_rcnn_test.pt \
   --net $1 \
   --cfg experiments/cfgs/faster_rcnn_alt_opt.yml \
   --input $2 \
