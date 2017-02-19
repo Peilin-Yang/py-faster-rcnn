4607b79f08e30a769b0b41f0e23ec17cab043bdb
Peilin Yang <yangpeilyn@gmail.com>
2017-02-18 22:54:52 -0500

    add alt_opt detect

--- lib/datasets/bib_detect.py
+++ lib/datasets/bib_detect.py
@@ -46,11 +46,14 @@ class bib_detect(imdb):
             image_path = os.path.join(self._source_folder, 
                     label,
                     index + self._image_ext)
-        except:
-            image_path = os.path.join(self._source_folder, 
-                    index + self._image_ext)
         if os.path.exists(image_path):
             return image_path
+
+        image_path = os.path.join(self._source_folder, 
+                index + self._image_ext)
+        if os.path.exists(image_path):
+            return image_path
+        
         print('image %s does exist....' % image_path)
         return None
 
